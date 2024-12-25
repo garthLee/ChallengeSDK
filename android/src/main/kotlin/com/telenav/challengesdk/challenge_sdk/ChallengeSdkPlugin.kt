423bc7b432c2c4012687b073ae359dc08066dd42
Li, Garth <qgli@telenavsoftware.com>
2024-12-25 14:43:49 +0800

    init gradle

--- android/src/main/kotlin/com/telenav/challengesdk/challenge_sdk/ChallengeSdkPlugin.kt
+++ android/src/main/kotlin/com/telenav/challengesdk/challenge_sdk/ChallengeSdkPlugin.kt
@@ -23,7 +23,7 @@ class ChallengeSdkPlugin: FlutterPlugin, MethodCallHandler {
 
   override fun onMethodCall(call: MethodCall, result: Result) {
     if (call.method == "getPlatformVersion") {
-      result.success("Android ${android.os.Build.VERSION.RELEASE}")
+      result.success("Android ${android.os.Build.VERSION.RELEASE} after change")
     } else {
       result.notImplemented()
     }
